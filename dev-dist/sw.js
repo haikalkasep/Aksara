d094bc186184659cbf5732d949ac958fe629eb37
Ririena <nerllysurina@gmail.com>
2024-03-03 20:17:22 +0700

    Random Option Value

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b6b5r9ntk8g"
+    "revision": "0.abb06d7ehpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

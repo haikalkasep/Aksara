03167113a0b04e5f1bd7ab2e5ab7b6e186b3e76d
Nabalzam <nabhan.alzam@gmail.com>
2024-03-03 15:15:45 +0700

    Artikel A - Selesai

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t1mp9l6t0ug"
+    "revision": "0.ju0lu55207g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Artikel/ArtikelVokal/ArtikelA.jsx
+++ src/components/Artikel/ArtikelVokal/ArtikelA.jsx
@@ -17,6 +17,8 @@ import {
 import Back from "../Back.jsx";
 import A from "../../../../public/images/AAksara.png";
 import Abdi from "../../../../public/images/Abdi.jpg";
+import Abdas from "../../../../public/images/AbdasAksara.png";
+import Vokalisasi from "../../../../public/images/avokal.png";
 
 export default function ArtikelA() {
   return (
@@ -29,19 +31,35 @@ export default function ArtikelA() {
           <Image src={A} className="w-44" />
           <h1 className="text-center mb-3 text-1xl">
             Cara Pengucapan :<br />
-            <h1 className="font-bold">A</h1>
+            <h1 className="font-bold text-2xl">a</h1>
           </h1>
         </Card>
       </div>
       <div className="flex justify-center mt-5">
         <Card>
-            <h1 className="mx-5 mt-3">Contoh Penggunaan :</h1>
+          <h1 className="mx-5 mt-3">Bentuk Vokalisasi :</h1>
+          <Image src={Vokalisasi} className="w-80" />
+        </Card>
+      </div>
+      <div className="flex justify-center mt-5 mb-24">
+        <Card>
+          <h1 className="mx-5 mt-3">Contoh Penggunaan :</h1>
+          <div className="mt-5">
           <Image src={Abdi} className="w-80" />
           <h1 className="text-center mt- mb-5 text-2xl font-bold">
             <div className="flex justify-center">
-              <h1 className="mr-5">a</h1> <h1 className="mx-16">b</h1> <h1 className="ml-5">di</h1>
+              <h1 className="">a</h1> <h1 className="mx-10">b</h1>{" "}
+              <h1 className="">di</h1>
             </div>
           </h1>
+          <Image src={Abdas} className="w-80" />
+          <h1 className="text-center mt- mb-5 text-2xl font-bold">
+            <div className="flex justify-center">
+              <h1 className="mr-10">a</h1> <h1 className="mr-5">b</h1>{" "}
+              <h1 className="mr-7 ml-6">da</h1> <h1>s</h1>
+            </div>
+          </h1>
+          </div>
         </Card>
       </div>
     </>

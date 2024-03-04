b892654f145345700adeb6af2e0d0916a7d1ca95
haikalkasep <147136705+haikalkasep@users.noreply.github.com>
2024-03-04 08:32:50 +0700

    Create ArtikelP.jsx

---
+++ src/pages/Artikels/ArtikelVokal/ArtikelP.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+} from "@nextui-org/react";
+import Back from "../../../components/Artikel/Back.jsx";
+import P from "../../../../public/images/P.png";
+import éndah from "../../../../public/images/endah.png";
+import éleh from "../../../../public/images/kalah.png";
+import Vokalisasi from "../../../../public/images/vokall.png";
+
+export default function ArtikelA() {
+  return (
+    <>
+      <div className="flex mb-5">
+        <Back />
+      </div>
+      <div className="flex justify-center">
+        <Card className="">
+          <Image src={P} className="w-44" />
+          <h1 className="text-center mb-3 text-1xl">
+            Cara Pengucapan :<br />
+            <h1 className="font-bold text-2xl">é</h1>
+          </h1>
+        </Card>
+      </div>
+      <div className="flex justify-center mt-5">
+        <Card>
+          <h1 className="mx-5 mt-3">Bentuk Vokalisasi :</h1>
+          <Image src={Vokalisasi} className="w-80" />
+        </Card>
+      </div>
+      <div className="flex justify-center mt-5 mb-24">
+        <Card>
+          <h1 className="mx-5 mt-3">Contoh Penggunaan :</h1>
+          <div className="mt-5">
+          <Image src={éndah} className="w-80" />
+          <h1 className="text-center mt- mb-5 text-2xl font-bold">
+            <div className="flex justify-center">
+              <h1 className="mr-8">e</h1> <h1 className="mr-10 nl-5">n</h1>{" "}
+              <h1 className="mr-3 nl-1">dah</h1>
+            </div>
+          </h1>
+          <Image src={éleh} className="w-80" />
+          <h1 className="text-center mt- mb-5 text-2xl font-bold">
+            <div className="flex justify-center">
+              <h1 className="mr-12 ml-6">é</h1> <h1 className="ml- mr-6">léh</h1>{" "}
+            </div>
+          </h1>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+}
